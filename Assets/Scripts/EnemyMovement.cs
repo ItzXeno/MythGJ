0929c26d767dfe5bf4f82963f1ff710e91b0ea7c
Zodiac411 <thechriszone@icloud.com>
2024-02-17 19:40:25 +0000

    Day 1
    added edge detection,
    spawning enemies
    enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    #region Fields and Properties
+    public enum MovementType { Vertical, Horizontal, Zigzag }
+    public MovementType movementType;
+
+    [SerializeField] private float minSpeed = 2f;
+    [SerializeField] private float maxSpeed = 5f;
+    private float speed;
+   
+    [SerializeField]
+    private float zigzagFrequency = 2f;
+    [SerializeField]
+    private float zigzagMagnitude = 0.5f;
+
+    // For zigzag movement calculation
+    private Vector3 axis;
+    private Vector3 direction;
+    #endregion
+
+    #region Unity Methods
+    void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+
+        // Set initial direction based on spawn side for horizontal and zigzag enemies
+        switch (movementType)
+        {
+            case MovementType.Horizontal:
+                direction = transform.position.x < 0 ? Vector3.right : Vector3.left;
+                break;
+            case MovementType.Zigzag:
+                axis = transform.position.y > 0 ? Vector3.down : (transform.position.x > 0 ? Vector3.left : Vector3.right);
+                direction = axis == Vector3.down ? Vector3.down : (axis == Vector3.left ? Vector3.left : Vector3.right);
+                break;
+            default:
+                direction = Vector3.down;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        switch (movementType)
+        {
+            case MovementType.Vertical:
+                MoveVertical();
+                break;
+            case MovementType.Horizontal:
+                MoveHorizontal();
+                break;
+            case MovementType.Zigzag:
+                MoveZigzag();
+                break;
+        }
+        if (IsOffScreen())
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    #region Movement Methods
+    void MoveVertical()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+    }
+
+    void MoveHorizontal()
+    {
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    }
+
+    void MoveZigzag()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        if (direction == Vector3.down)
+        {
+            transform.position += Vector3.right * Mathf.Sin(Time.time * zigzagFrequency) * zigzagMagnitude;
+        }
+        else
+        {
+            transform.position += Vector3.down * Mathf.Sin(Time.time * zigzagFrequency) * zigzagMagnitude;
+        }
+    }
+
+    bool IsOffScreen()
+    {
+        Vector2 screenPosition = Camera.main.WorldToViewportPoint(transform.position);
+        // Check if the object is outside the viewport
+        // Note: 0,0 is the bottom left of the screen, 1,1 is the top right
+        return screenPosition.x < 0 || screenPosition.x > 1 || screenPosition.y < 0 || screenPosition.y > 1;
+    }
+    #endregion
+
+    #region Inspector Methods
+    // This region can be used for methods that are specifically meant to be called or shown in the Unity Inspector if needed in the future.
+    #endregion
+}
